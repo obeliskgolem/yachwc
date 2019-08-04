96e7aae619b59af56a75006403aa24308d82c876
jave <jave@jave.local>
2019-08-04 11:19:09 +0800

    stack

--- Main.hs
+++ Main.hs
@@ -10,19 +10,22 @@ import Data.Text.Encoding
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as BSL
 
+import Data.List
+
 type URL        = String
 type InnerText  = T.Text
 type Depth      = Int
-type URLInfo    = (InnerText, Depth)
+type URLInfo    = (URL, InnerText)
 
-data URLMap     = Map URL URLInfo
+data URLMap     = Map URL Int
         deriving Show
 
 websiteURL :: URL
-websiteURL = "https://obeliskgolem.github.io/"
+-- websiteURL = "https://obeliskgolem.github.io/"
+websiteURL = "https://obeliskgolem.github.io/posts/2019-02-28-building-hakyll-site-1.html"
 
-websiteInfo :: URLInfo
-websiteInfo = ("首页", 0)
+--websiteInfo :: URLInfo
+--websiteInfo = ("首页", 0)
 
 main :: IO ()
 main = do
@@ -30,7 +33,38 @@ main = do
 --        processMap websiteURL websiteInfo Map.empty
         return ()
 
+-------------- URL Processing --------------
+makeAbsoluteURL :: String -> String -> String
+makeAbsoluteURL root rel 
+        | "http://" `isPrefixOf` rel    = rel
+        | "https://" `isPrefixOf` rel   = rel
+        | otherwise                     = root ++ ('/':rel)
+
+data URLAction = None | Pop | Push String
+
+generateActions :: String -> URLAction
+generateActions ".."   = Pop
+generateActions "."    = None
+generateActions ""     = None
+generateActions s      = Push s
+
+makeValidURL :: [String] -> [String]
+makeValidURL (x:_:"..":xs) = makeValidURL (x:xs)
+makeValidURL (".":xs) = makeValidURL xs
+makeValidURL ("":xs) = makeValidURL xs
+makeValidURL (x:xs) = x:(makeValidURL xs)
+makeValidURL [] = []
+
 -------------- Processing --------------
+findHref :: [Attr] -> String
+findHref [] = []
+findHref (Attr "href" s:xs) = T.unpack s
+findHref (x:xs) = findHref xs
+
+findAllHrefs :: [Token] -> [URLInfo]
+findAllHrefs (TagOpen "a" attrs:ContentText t:xs) = (findHref attrs, t):(findAllHrefs xs)
+findAllHrefs [] = []
+findAllHrefs (x:xs) = findAllHrefs xs
 
 
 -------------- Testing          --------
@@ -43,5 +77,5 @@ testHTTP = do
     req <- parseRequest websiteURL
     response <- httpLbs req man 
     let parsed_tokens   = parseTokens $ decodeUtf8 $ BSL.toStrict $ responseBody response
-    print $ parsed_tokens
+    print $ findAllHrefs parsed_tokens
     return ()
