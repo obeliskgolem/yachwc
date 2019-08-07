23764a9a6a28d7eebd27cc140c193b7ae28fc5f3
obeliskgolem <obelisk.golem@gmail.com>
2019-08-07 16:33:44 +0800

    add map lookup

--- Main.hs
+++ Main.hs
@@ -5,32 +5,48 @@ import Network.HTTP.Client.TLS
 
 import System.Environment
 
-import Control.Concurrent.Chan.Unagi
+import qualified Control.Concurrent.Chan.Unagi as UChan
 
 import Text.HTML.Parser
 import Data.Text.Encoding
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as BSL
 
+import Control.Concurrent
+import Control.Concurrent.Async
+import Control.Monad
+
+import Data.Maybe
 import Data.List
 import qualified Data.Map.Strict as Map
 
 type URL        = String
 type InnerText  = T.Text
 type Depth      = Int
-type URLInfo    = (URL, InnerText)
+type Info       = (InnerText, Depth)
+type URLInfo    = (URL, Info)
 
-data URLMap     = Map URL Int
-        deriving Show
-
-websiteURL :: URL
 websiteURL = "https://obeliskgolem.github.io/posts/2019-02-28-building-hakyll-site-1.html"
+rootURL = (websiteURL, ("首页", 0))
+maxDepth = 1
 
 main :: IO ()
 main = do
-        (in_c, out_c) <- newChan 
-        testHTTP
+        (in_c, out_c) <- UChan.newChan
+        UChan.writeChan in_c rootURL
+
+        globalMap <- newEmptyMVar
+        putMVar globalMap (Map.fromList [rootURL])
+
+        testHTTP in_c rootURL globalMap
+        --forever $ (readChan out_c) (forkIO $ testHTTP in_c rootURL globalMap)
+        m <- takeMVar globalMap
+        print $ Map.toList m
         return ()
+
+------- Global HTTP Client Settings --------
+globalHTTPSetting = managerSetProxy (proxyEnvironment Nothing) defaultManagerSettings
+globalHTTPSSetting = managerSetProxy (proxyEnvironment Nothing) tlsManagerSettings
 
 -------------- URL Processing --------------
 makeAbsoluteURL :: String -> String -> String
@@ -51,28 +67,32 @@ findHref [] = []
 findHref (Attr "href" s:xs) = T.unpack s
 findHref (x:xs) = findHref xs
 
-findAllHrefs :: String -> [Token] -> [URLInfo]
-findAllHrefs url (TagOpen "a" attrs:ContentText t:xs) = if x /= "" then
-        (makeAbsoluteURL url x, t):(findAllHrefs url xs)
+findAllHrefs :: URLInfo -> [Token] -> [URLInfo]
+findAllHrefs url_info@(url, (_, d)) (TagOpen "a" attrs:ContentText t:xs) = if x /= "" then
+        ((makeAbsoluteURL url x, (t, d+1))):(findAllHrefs url_info xs)
         else
-                findAllHrefs url xs
+                findAllHrefs url_info xs
         where x = findHref attrs
 findAllHrefs _ [] = []
-findAllHrefs url (x:xs) = findAllHrefs url xs
-
+findAllHrefs url_info (x:xs) = findAllHrefs url_info xs
 
 -------------- Testing          --------
-testHTTP :: IO ()
-testHTTP = do
-    let settings = managerSetProxy
-            (proxyEnvironment Nothing)
-            tlsManagerSettings
-    man <- newManager settings
-    req <- parseRequest websiteURL
+testHTTP :: UChan.InChan URLInfo -> URLInfo -> MVar (Map.Map URL Info) -> IO ()
+testHTTP in_c url_info@(url, (_, d)) gMap = do
+--    if ("https://" `isPrefixOf` URL) then
+--        man <- newManager globalHTTPSSetting
+--    else
+--        man <- newManager globalHTTPSetting
+    when (d >= maxDepth) (return ())
+
+    man <- newManager globalHTTPSSetting
+    req <- parseRequest url
     response <- httpLbs req man
+    m <- takeMVar gMap
+
     let parsed_tokens   = parseTokens $ decodeUtf8 $ BSL.toStrict $ responseBody response
-    let page_urls = findAllHrefs websiteURL parsed_tokens
-    writeList2Chan in_c page_urls
-    getChanContents out_c >>= print
-    --putStrLn $ show $ findAllHrefs parsed_tokens
+    let page_urls = findAllHrefs rootURL parsed_tokens
+    let new_urls = filter (isNothing . (`Map.lookup` m) . fst) page_urls
+    UChan.writeList2Chan in_c new_urls
+    putMVar gMap (Map.union m (Map.fromList new_urls))
     return ()
